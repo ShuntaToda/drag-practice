21878b8b6decc78706de33ab7406ecfa34d46010
todashunta <todashuntawork@gmail.com>
2022-10-05 22:59:03 +0900

    task drag

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { dummyData } from "./dummyData";
 
 function App() {
+  console.log(dummyData);
+  const onDragEnd = () => {};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <DragDropContext onDragEnd={onDragEnd}>
+        {dummyData.map((board, boardIndex) => (
+          <div className="card" key={board.id}>
+            <h2 className="card-header ">{board.name}</h2>
+            <div className="card-body d-flex overflow-auto">
+              {/* カテゴリー */}
+              {board.categories.map((category, categoryIndex) => (
+                <div className="card" key={category.id}>
+                  <h3 className="card-header">{category.name}</h3>
+                  <Droppable droppableId={categoryIndex.toString()} type="task">
+                    {(provided, snapshot) => (
+                      <div
+                        className="card-body p-4"
+                        ref={provided.innerRef}
+                        {...provided.droppableProps}
+                      >
+                        {/* タスク */}
+                        {category.tasks.map((task, taskIndex) => (
+                          <Draggable
+                            key={task.id}
+                            draggableId={task.id.toString()}
+                            index={taskIndex}
+                          >
+                            {(provided, snapshot) => (
+                              <h4
+                                className="border p-3"
+                                ref={provided.innerRef}
+                                {...provided.draggableProps}
+                                {...provided.dragHandleProps}
+                              >
+                                {task.name}
+                              </h4>
+                            )}
+                          </Draggable>
+                        ))}
+                        {provided.placeholder}
+                      </div>
+                    )}
+                  </Droppable>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </DragDropContext>
     </div>
   );
 }
---
+++ src/dummyData.js
@@ -0,0 +1,40 @@
+export const dummyData = [
+  {
+    id: 1,
+    name: "ボード名",
+    categories: [
+      {
+        id: 2,
+        name: "カテゴリー名",
+        tasks: [
+          {
+            id: 3,
+            name: "タスク名1",
+          },
+          {
+            id: 4,
+            name: "タスク名2",
+          },
+          {
+            id: 5,
+            name: "タスク名3",
+          },
+        ],
+      },
+      {
+        id: 6,
+        name: "カテゴリー名",
+        tasks: [
+          {
+            id: 7,
+            name: "タスク名1",
+          },
+          {
+            id: 8,
+            name: "タスク名2",
+          },
+        ],
+      },
+    ],
+  },
+];
